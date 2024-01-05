3fe61a4b5b423163224eba021df32fe114209e0b
tanvirislamrakib <155340897+tanvirislamrakib@users.noreply.github.com>
2024-01-05 18:00:02 +0600

    Add files via upload

---
+++ function.js
@@ -0,0 +1,60 @@
+function Fun(){
+    var num1 = 10;
+    var num2 = 10;
+    console.log(num1 * num2)
+}
+Fun()
+Fun()
+//console.log("Hello World")
+Fun()
+//console.log("Aso sobay khucuri khay")
+Fun()
+
+
+
+function number(num1, num2){
+    var total = num1 + num2;
+    console.log(total)
+}
+number(10, 20)
+
+
+function fun(num1, num2){
+    console.log(num1 * num2)
+}
+fun(2, 3)
+
+function funn(num1, num2){
+    console.log(num1 ** num2)
+}
+funn(2, 3)
+
+
+
+
+function rakib(num1){
+    var total = num1 *10;
+    return total;
+}
+var a = rakib(10);
+var b = rakib(20);
+console.log(a);
+
+
+
+
+function rakib(number1){
+    var total = number1 * 10;
+    return total;
+}
+var a = rakib(20);
+console.log(a);
+
+
+
+function china(name){
+    var mot = name +100;
+    return mot;
+}
+var x = china(30);
+console.log(x)
